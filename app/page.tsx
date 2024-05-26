b53973b5d88a23b8337da39fb229885d37b8e82b
Kate-Chua <katejolie100@gmail.com>
2024-05-25 19:16:52 -0700

    final ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <>
@@ -6,14 +8,22 @@ export default function Home() {
           <h1 className="text-[#EEEEEE] text-[150px] italic fade-up-right">The Truth Project</h1>
           <p className="py-2 w-2/3 text-center text-2xl leading-loose">Empower yourself with knowledge. Our website detects the political bias in the news you consume and offers concise summaries, helping you stay informed without bias.</p>
         </div>
-        <div className="bg-white h-screen w-screen">
-          <h1 className="text-6xl text-center pt-16">Summarizer</h1>
-          <input type="text" placeholder="test" />
-          <form action="/api/summarize" method="POST">
-            <label htmlFor="url">Enter article URL:</label>
-            <input type="text" id="url" name="url" required />
-            <button type="submit">Summarize</button>
-          </form>
+        <div className="bg-white h-screen w-screen flex flex-row items-start pt-16 justify-center">
+          <div className="pl-12">
+            <h1 className="text-6xl text-left">Get Your News, Quick.</h1>
+            <form action="/api/summarize" method="POST">
+              <p className="text-2xl leading-loose py-2 w-1/2">Paste a URL link, hit `enter`, and watch the magic happen. Our Summarizer will provide a concise and accurate summary of any political article so you can educate yourself in mere seconds.</p>
+              <label htmlFor="url">Enter article URL:</label>
+              <input type="text" id="url" name="url" required />
+            </form>
+          </div>
+          <Image
+            className="pr-12"
+            src="/image.png"
+            width={500}
+            height={500}
+            alt="Image"
+          />
         </div>
       </div>
     </>
